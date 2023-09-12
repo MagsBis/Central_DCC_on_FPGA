da6d5b06615e5be1875debeecdce73721740f932
MagsBis <143169593+MagsBis@users.noreply.github.com>
2023-09-12 21:27:03 +0200

    Update MAE.vhd

--- src/vhd code/MAE.vhd
+++ src/vhd code/MAE.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: SORBONNE UNIVERSITE
--- Designed by: BISLIEV Magomed 
+-- Designed by: BISLIEV Magomed - YANG Zixiao
 --
 -- Module Name: MAE - Behavioral
 -- Project Name: Centrale DCC
