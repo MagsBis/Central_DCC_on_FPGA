c7ba9a48e8a7f11470d7ba66c13d9d21e3d8011d
MagsBis <143169593+MagsBis@users.noreply.github.com>
2023-09-12 21:29:22 +0200

    Update TB_COMPTEUR_TEMPO.vhd

--- sim/TB_COMPTEUR_TEMPO.vhd
+++ sim/TB_COMPTEUR_TEMPO.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: SORBONNE UNIVERSITE
--- Designed by: BISLIEV Magomed 
+-- Designed by: BISLIEV Magomed - YANG Zixiao
 --
 -- Module Name: TB_COMPTEUR_TEMPO - Behavioral
 -- Project Name: Centrale DCC
