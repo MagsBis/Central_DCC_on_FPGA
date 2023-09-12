04fef7b0c6e9d6d6f5b62a31feda12d1e624a75a
MagsBis <143169593+MagsBis@users.noreply.github.com>
2023-09-12 21:30:38 +0200

    Update TB_registre_dcc.vhd

--- sim/TB_registre_dcc.vhd
+++ sim/TB_registre_dcc.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company: SORBONNE UNIVERSITE
--- Designed by: BISLIEV Magomed 
+-- Designed by: BISLIEV Magomed - YANG Zixiao
 --
 -- Module Name: TB_registre_dcc - Behavioral
 -- Project Name: Centrale DCC
